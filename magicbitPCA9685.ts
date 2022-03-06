4979704c6d1dc254fbd443c2acfb48b821c031e3
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2022-03-06 16:30:12 +0100

    servo and motor fn

---
+++ magicbitPCA9685.ts
@@ -0,0 +1,359 @@
+//% color="#EE6A50" icon="\uf013" block="Magic:bit PCAmotor" blockId="PCAmotor"
+namespace PCAmotor {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
+
+    export enum Motors {
+        //% block="M1"
+        M1 = 0x3,
+        //% block="M2"
+        M2 = 0x4,
+        //% block="M3"
+        M3 = 0x1,
+        //% block="M4"
+        M4 = 0x2
+    }
+
+    export enum Steppers {
+        //% block="STPM1"
+        STPM1 = 0x2,
+        //% block="STPM2"
+        STPM2 = 0x1
+    }
+
+    export enum Servos {
+        //% block="S1"
+        S1 = 0x01,
+        //% block="S2"
+        S2 = 0x02,
+        //% block="S3"
+        S3 = 0x03,
+        //% block="S4"
+        S4 = 0x04,
+        //% block="S5"
+        S5 = 0x05,
+        //% block="S6"
+        S6 = 0x06,
+        //% block="S7"
+        S7 = 0x07,
+        //% block="S8"
+        S8 = 0x08
+    }
+
+    let initialized = false
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        for (let idx = 0; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        //serial.writeValue("ch", channel)
+        //serial.writeValue("on", on)
+        //serial.writeValue("off", off)
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+    function setStepper(index: number, dir: boolean): void {
+        if (index == 1) {
+            if (dir) {
+                setPwm(0, STP_CHA_L, STP_CHA_H);
+                setPwm(2, STP_CHB_L, STP_CHB_H);
+                setPwm(1, STP_CHC_L, STP_CHC_H);
+                setPwm(3, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(3, STP_CHA_L, STP_CHA_H);
+                setPwm(1, STP_CHB_L, STP_CHB_H);
+                setPwm(2, STP_CHC_L, STP_CHC_H);
+                setPwm(0, STP_CHD_L, STP_CHD_H);
+            }
+        } else {
+            if (dir) {
+                setPwm(4, STP_CHA_L, STP_CHA_H);
+                setPwm(6, STP_CHB_L, STP_CHB_H);
+                setPwm(5, STP_CHC_L, STP_CHC_H);
+                setPwm(7, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(7, STP_CHA_L, STP_CHA_H);
+                setPwm(5, STP_CHB_L, STP_CHB_H);
+                setPwm(6, STP_CHC_L, STP_CHC_H);
+                setPwm(4, STP_CHD_L, STP_CHD_H);
+            }
+        }
+    }
+
+    function stopMotor(index: number) {
+        setPwm((index - 1) * 2, 0, 0);
+        setPwm((index - 1) * 2 + 1, 0, 0);
+    }
+
+    //% blockId=magicbit_stepper_degree block="Stepper 28BYJ-48|%index|degree %degree"
+    //% weight=90
+    export function StepperDegree(index: Steppers, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setStepper(index, degree > 0);
+        degree = Math.abs(degree);
+        basic.pause(10240 * degree / 360);
+        MotorStopAll()
+    }
+
+    /**
+     * Stepper Car move forward
+     * @param distance Distance to move in cm; eg: 10, 20
+     * @param diameter diameter of wheel in mm; eg: 48
+    */
+    //% blockId=magicbit_stpcar_move block="Car Forward|Distance(cm) %distance|Wheel Diameter(mm) %diameter"
+    //% weight=88
+    export function StpCarMove(distance: number, diameter: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        let delay = 10240 * 10 * distance / 3 / diameter; // use 3 instead of pi
+        setStepper(1, delay > 0);
+        setStepper(2, delay > 0);
+        delay = Math.abs(delay);
+        basic.pause(delay);
+        MotorStopAll()
+    }
+
+    //% blockId=magicbit_motor_run block="Motor|%index|speed %speed"
+    //% weight=85
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+    //% blockId=magicbit_stop block="Motor Stop|%index|"
+    //% weight=80
+    export function MotorStop(index: Motors): void {
+        MotorRun(index, 0);
+    }
+
+    //% blockId=magicbit_stop_all block="Motor Stop All"
+    //% weight=79
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        for (let idx = 1; idx <= 4; idx++) {
+            stopMotor(idx);
+        }
+    }
+
+    /**
+     * Servo Execute
+     * @param index Servo Channel; eg: PCAservo.Servos.S1
+     * @param degree [0-180] degree of servo; eg: 0, 90, 180
+    */
+    //% blockId=magicbit_servo block="Servo|%index|degree %degree"
+    //% weight=100
+    //% degree.min=0 degree.max=180
+    export function Servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+    /**
+      * Servo Execute (from-to)
+      * @param index Servo Channel; eg: PCAservo.Servos.S1
+      * @param degree1 [0-180] degree of servo; eg: 0, 90, 180
+      * @param degree2 [0-180] degree of servo; eg: 0, 90, 180
+      * @param speed [1-10] speed of servo; eg: 1, 10
+     */
+    //% blockId=motorbit_servospeed block="Servo|%index|degree start %degree1|end %degree2|speed %speed"
+    //% weight=98
+    //% degree1.min=0 degree1.max=180
+    //% degree2.min=0 degree2.max=180
+    //% speed.min=1 speed.max=10
+    //% inlineInputMode=inline
+    export function Servospeed(index: Servos, degree1: number, degree2: number, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        if (degree1 > degree2) {
+            for (let i = degree1; i > degree2; i--) {
+                let v_us = (i * 1800 / 180 + 600) // 0.6 ~ 2.4
+                let value = v_us * 4096 / 20000
+                basic.pause(4 * (10 - speed));
+                setPwm(index + 7, 0, value)
+            }
+        }
+        else {
+            for (let i = degree1; i < degree2; i++) {
+                let v_us = (i * 1800 / 180 + 600) // 0.6 ~ 2.4
+                let value = v_us * 4096 / 20000
+                basic.pause(4 * (10 - speed));
+                setPwm(index + 7, 0, value)
+            }
+        }
+    }
+
+    /**
+     * Geek Servo Execute
+     * @param index Servo Channel; eg: PCAservo.Servos.S1
+     * @param pwm pulse width [500-2500] in ms of servo; eg: 500, 1500, 2500
+    */
+    //% blockId=magicbit_geekservo block="Servo|%index|pulse width %v_us"
+    //% weight=90
+    //% v_us.min=300 v_us.max=2800
+    export function GeekServo(index: Servos, v_us: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let value = Math.constrain(v_us, 300, 2800) * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+    /**
+      * Geek Servo Execute (from-to)
+      * @param index Servo Channel; eg: PCAservo.Servos.S1
+      * @param pwm1 [300-2800];
+      * @param pwm2 [300-2800];
+      * @param speed [1-10] speed of servo; eg: 1, 10
+     */
+    //% blockId=motorbit_gsrvspeed block="Servo|%index|pulse start %pwm1|end %pwm2|speed %speed"
+    //% weight=87
+    //% pwm1.min=400 pwm1.max=2600
+    //% pwm2.min=400 pwm2.max=2600
+    //% speed.min=1 speed.max=10
+    //% inlineInputMode=inline
+    export function GeekServospeed(index: Servos, pwm1: number, pwm2: number, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        pwm1 = Math.round(pwm1 / 10) * 10
+        pwm2 = Math.round(pwm2 / 10) * 10
+        // 50hz: 20,000 us
+        if (pwm1 > pwm2) {
+            for (let i = pwm1; i >= pwm2; i-=10) {
+                let value = i * 4096 / 20000
+                basic.pause(4 * (10 - speed));
+                setPwm(index + 7, 0, value)
+            }
+        }
+        else {
+            for (let i = pwm1; i <= pwm2; i+=10) {
+                let value = i * 4096 / 20000
+                basic.pause(4 * (10 - speed));
+                setPwm(index + 7, 0, value)
+            }
+        }
+    }
+
+    /**
+     * Release servo
+     * @param index Servo Channel; eg: PCAservo.Servos.S1
+    */
+    //% blockId=magicbit_stopservo block="Servo|%index|"
+    //% weight=80
+    export function StopServo(index: Servos): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setPwm(index + 7, 0, 0)
+    }
+}
---
+++ servo.ts
@@ -0,0 +1,49 @@
+class Servo {
+    private _up: number
+    private _down: number
+    private _center: number
+    private _pulse: number
+    private _pin: PCAmotor.Servos
+    private _speed: number
+
+    constructor(pin: PCAmotor.Servos, up: number, down: number, center: number = 1500, speed: number = 10) {
+        //super(pin, initPulse)
+        PCAmotor.GeekServo(pin, center)
+        this._pin = pin
+        this._up = up
+        this._down = down
+        this._pulse = center
+        this._center = center
+        this._speed = speed
+    }
+
+    public up(): void {
+        this.setPulse(this._up, this._speed)
+    }
+    public center(): void {
+        this.setPulse(this._center, this._speed)
+    }
+    public down(): void {
+        this.setPulse(this._down, this._speed)
+    }
+    public stop(): void {
+        PCAmotor.StopServo(this._pin)
+    }
+    public isDown(): boolean {
+        return Math.abs(this._pulse - this._down) <= 150
+    }
+    public setPulse(pulse: number, speed: number = 10) {
+        pulse = Math.constrain(pulse, Math.min(this._down, this._up), Math.max(this._down, this._up))
+        //console.logValue(this._pin, pulse)
+        if (speed == 0) {
+            PCAmotor.GeekServo(this._pin, pulse)
+        } else
+            PCAmotor.GeekServospeed(this._pin, this._pulse, pulse, speed)
+        this._pulse = pulse
+    }
+    public changePulse(us: number, speed: number = 4) {
+        this.setPulse(this._pulse + us, speed)
+        if (this._pulse == this._up || this._pulse == this._down || this._pulse == this._center)
+            this.stop()
+    }
+}
